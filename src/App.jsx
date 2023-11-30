abab23d026fc4434d5a7c99ec3ae58dbb2e6aebb
sphnikola <sphnikola@gmail.com>
2023-11-30 09:25:10 +0000

    removed the header component and decided to use only the notification component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./Header";
+import { Notification } from "./Notification";
+
+function App() {
+  return (
+    <>
+      <div
+        className="max-w-[730px] bg-white rounded-2xl md:mx-auto font-plus px-3 md:px-6
+      "
+      >
+        {/* <Header /> */}
+        <Notification />
+      </div>
+    </>
+  );
+}
+
+export default App;
